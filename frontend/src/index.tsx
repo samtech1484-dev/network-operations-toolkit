27d035936427fa0da8f26f3459e0fd215f3bb7d4
samtech1484-dev <samtech1484@gmail.com>
2026-05-30 23:15:29 +0530

    Add frontend index.tsx

---
+++ frontend/src/index.tsx
@@ -0,0 +1,1 @@
+import React from 'react';\nimport ReactDOM from 'react-dom/client';\nimport App from './App';\nimport './index.css';\n\nconst root = ReactDOM.createRoot(\n  document.getElementById('root') as HTMLElement\n);\nroot.render(\n  <React.StrictMode>\n    <App />\n  </React.StrictMode>\n);\n
